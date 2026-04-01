b97186356d2fa063482e0916f01a54b65feb2ac9
6kts0 <fabakouyate06@gmail.com>
2026-04-01 00:27:56 -0700

    Integer declaration without memory allocation

---
+++ memorymgmt/NoAlloc/noHeaploc.c
@@ -0,0 +1,10 @@
+// Initializing an integer with no memory allocation
+#include <stdlib.h>
+#include <stdio.h>
+
+int main() {
+    int P = 42;
+
+    printf("P Memory Address: %p \n", P);
+    printf("P: %d \n", P);
+}
